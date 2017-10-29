d49b5516dfa6e7609333540102664a854c65f4d0
Harlley Lima <harlleyaugusto@gmail.com>
2017-10-29 13:59:58 -0200

    before sorting after included at RankingAggregation

--- src/ufmg/dcc/rm/rankaggreg/CrossEntropyMonteCarlo.java
+++ src/ufmg/dcc/rm/rankaggreg/CrossEntropyMonteCarlo.java
@@ -5,17 +5,43 @@ import java.util.HashMap;
 
 import ufmg.dcc.rm.parse.DataReaderFromView;
 import ufmg.dcc.rm.parse.FileParserContext;
+import ufmg.dcc.rm.qa.Answer;
 import ufmg.dcc.rm.qa.Question;
 
-public class CrossEntropyMonteCarlo {
+public class CrossEntropyMonteCarlo extends RankingAggregation {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-
-		FileParserContext fpc = new FileParserContext(new DataReaderFromView());
-		HashMap<Integer, Question> questions = fpc.parse();
+	public CrossEntropyMonteCarlo() {
+		super();
 	}
 
+	@Override
+	public void before() throws FileNotFoundException {
+		forum = fpc.parse();
+		Question.sortingAllAnswer(forum);
+	}
 
+	@Override
+	public void sorting() {
+		// TODO Auto-generated method stub
+		for (Integer qid : forum.keySet()) {
+			for (String view : forum.get(qid).getRankingView().keySet()) {
+				
+			}
+		}
+	}
+
+	@Override
+	protected void after() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * public static void main(String[] args) throws FileNotFoundException { //
+	 * TODO Auto-generated method stub
+	 * 
+	 * FileParserContext fpc = new FileParserContext(new DataReaderFromView());
+	 * HashMap<Integer, Question> questions = fpc.parse(); }
+	 */
 
 }
--- src/ufmg/dcc/rm/rankaggreg/RankingAggregation.java
+++ src/ufmg/dcc/rm/rankaggreg/RankingAggregation.java
@@ -1,7 +1,36 @@
 package ufmg.dcc.rm.rankaggreg;
 
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+import ufmg.dcc.rm.parse.DataReaderFromView;
+import ufmg.dcc.rm.parse.FileParserContext;
+import ufmg.dcc.rm.qa.Question;
+
 public abstract class RankingAggregation {
+	
+	
+	protected HashMap<Integer, Question> forum;
+	protected FileParserContext fpc; //
+	
+	protected abstract void before() throws FileNotFoundException;
+	protected abstract void sorting();
+	protected abstract void after();
+	
+	public RankingAggregation()
+	{
+		forum = new HashMap<Integer, Question>();
+		fpc = new FileParserContext(new DataReaderFromView());
+	}
+	
+	protected void run() throws FileNotFoundException
+	{
+		this.before();
+		this.sorting();
+		this.after();
+	}
+	
+	
 
 	
-	public abstract void sorting();
 }
